833b2097ae2282ba0717a1298c62488b4ef81f63
Austin Jackson <themacphage@gmail.com>
2021-08-03 18:40:33 -0500

    edit

---
+++ run-installer.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wine $HOME/iTunesSetup.exe
--- setup.sh
+++ setup.sh
@@ -8,10 +8,12 @@ winetricks -q gdiplus msls31 msxml3
 
 mkdir -p $HOME/Desktop \
   && cd $HOME/Desktop \
-  && wget -O iTunesSetup.exe \
+  && wget -O $HOME/iTunesSetup.exe \
     https://secure-appldnld.apple.com/itunes12/091-81691-20180709-C98EBAE5-D5C5-41EC-9847-A5F71D9C4437/iTunesSetup.exe
 
 wget -q -O setup.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/setup.sh
 chmod +x setup.sh
 wget -q -O open-itunes.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/open-itunes.sh
 chmod +x open-itunes.sh
+wget -q -O run-installer.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/run-installer.sh
+chmod +x run-installer.sh
