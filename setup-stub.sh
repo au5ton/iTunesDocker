75161bb3d8f1279673557b37bfd5e9b43efd269a
Austin Jackson <themacphage@gmail.com>
2021-08-03 18:42:21 -0500

    added stub

---
+++ setup-stub.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/bin/bash -c "$(wget -qO- https://raw.githubusercontent.com/au5ton/iTunesDocker/main/setup.sh)"
--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ mkdir -p $HOME/Desktop \
   && wget -O $HOME/iTunesSetup.exe \
     https://secure-appldnld.apple.com/itunes12/091-81691-20180709-C98EBAE5-D5C5-41EC-9847-A5F71D9C4437/iTunesSetup.exe
 
-wget -q -O setup.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/setup.sh
+wget -q -O setup.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/setup-stub.sh
 chmod +x setup.sh
 wget -q -O open-itunes.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/open-itunes.sh
 chmod +x open-itunes.sh
