ef73697c5fd4af4fb31147af8d1a233398a86ea6
Austin Jackson <themacphage@gmail.com>
2021-08-03 17:56:00 -0500

    changed mounts

--- run.ps1
+++ run.ps1
@@ -6,8 +6,9 @@ docker run -d `
  -e TZ=America/Chicago `
  -e RDP_SERVER=yes `
  -e WINEARCH=win32 `
- -v e:/Docker/iTunes/user.reg:/home/wineuser/.wine/user.reg:rw `
- -v e:/Docker/iTunes/iTunes:/home/wineuser/iTunes:rw `
+ -v "e:/Docker/iTunes/Program Files:/home/wineuser/.wine/drive_c/Program Files:rw" `
+ -v "e:/Docker/iTunes/ProgramData:/home/wineuser/.wine/drive_c/ProgramData:rw" `
+ -v e:/Docker/iTunes/Music:/home/wineuser/Music:rw `
  -v "e:/Docker/iTunes/appdata:/home/wineuser/.wine/drive_c/users/wineuser/Application Data:rw" `
  scottyhardy/docker-wine:latest
 
