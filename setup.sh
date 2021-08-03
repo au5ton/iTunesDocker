c7d4299814bfdba298f0de6bf2a1d539549df0d4
Austin Jackson <themacphage@gmail.com>
2021-08-03 17:46:36 -0500

    forgot this

--- setup.sh
+++ setup.sh
@@ -12,4 +12,6 @@ mkdir -p $HOME/Desktop \
     https://secure-appldnld.apple.com/itunes12/091-81691-20180709-C98EBAE5-D5C5-41EC-9847-A5F71D9C4437/iTunesSetup.exe
 
 wget -q -O setup.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/setup.sh
-wget -q -O open-itunes.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/open-itunes.sh 
+chmod +x setup.sh
+wget -q -O open-itunes.sh https://raw.githubusercontent.com/au5ton/iTunesDocker/main/open-itunes.sh
+chmod +x open-itunes.sh
